f39bd05e0952ac2080575e04499ea0392fdd1f99
Suraj Sonwane <surajsonwanefullstack@gmail.com>
2025-01-18 09:18:10 +0530

    portfolio page created

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express")
+const path = require("path")
 const {configDotenv} = require('dotenv')
 const app =express()
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static("/pages"))
 configDotenv({
-    path:"./.env"
+    path:"./env"
 })
 const PORT = process.env.PORT || 8001
 app.get("/",(req,res)=>{
-    res.send("welcome")
+    res.sendFile(path.join(__dirname, './pages/portfolio/portfolio.html'))
+})
+
+app.get("/linkedin",(req,res)=>{
+    res.sendFile(path.join(__dirname,'/pages/linkedin/linkedin.html'))
 })
 
 app.listen(PORT,()=>{
